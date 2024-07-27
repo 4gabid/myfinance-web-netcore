440d710d494aa18bdb80d116193eee26a57f56dd
SOUZA, GABRIELLE DESIREE (G.C.) <gsouza64@ford.com>
2024-07-27 19:19:50 -0300

    modelagem das entidades para mapear com a tabela do db

---
+++ 3- myfinance-web-netcore-domain/Entitities/Transacao.cs
@@ -0,0 +1,13 @@
+﻿namespace myfinance_web_netcore_domain.Entities;
+
+public class Transacao
+{
+    public int Id { get; set;}
+    public string Descricao { get; set;}
+    public string Tipo {g et; set;}
+    public string Historico3 { get; set;}
+    public DateTime Valor {g et; set;}
+    public int PlanoContaId { get; set;}
+    public PlanoConta PlanoConta { get; set; }
+
+}
--- 4- myfinance-web-netcore-infra/MyFinanceDbContext.cs
+++ 4- myfinance-web-netcore-infra/MyFinanceDbContext.cs
@@ -4,5 +4,12 @@ namespace myfinance_web_netcore_infra;
 
 public class MyFinanceDbContext : DbContext
 {
+    public DbSet<PlanoConta> PlanoConta {get; set;}
+    public DbSet<Transacao> Transacao {get; set;}
+
+    protected override void OnConfiguring(DbContetOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@ "Server=.SQLEXPRESS;Database=myfinance;Trusted_Connection=True;");
+    }
 
 }
